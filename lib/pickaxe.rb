dbacf0b9e98a16adcd7389c463deaa853753029e
Dawid Fatyga <dawid.fatyga@gmail.com>
2010-11-16 01:31:42 +0100

    Added dynamic with to word_wrap.

--- lib/pickaxe.rb
+++ lib/pickaxe.rb
@@ -13,7 +13,21 @@ module Pickaxe
     def self.status_code(code = nil)
       define_method(:status_code) { code }
     end
-  end  
+  end
+  
+  module Shell
+  	def self.dynamic_width
+      (dynamic_width_stty.nonzero? || dynamic_width_tput)
+    end
+
+    def self.dynamic_width_stty
+      %x{stty size 2>/dev/null}.split[1].to_i
+    end
+
+    def self.dynamic_width_tput
+      %x{tput cols 2>/dev/null}.to_i
+    end  
+  end
 end
 
 # TODO: use autoload
--- lib/pickaxe/extensions.rb
+++ lib/pickaxe/extensions.rb
@@ -2,9 +2,9 @@ class String
 	def word_wrap(*args)
 		options = args.extract_options!
 		unless args.blank?
-			options[:line_width] = args[0] || 80
+			options[:line_width] = args[0] || Pickaxe::Shell.dynamic_width || 80
 		end
-		options.reverse_merge!(:line_width => 80)
+		options.reverse_merge!(:line_width => Pickaxe::Shell.dynamic_width || 80)
 
 		self.split("\n").collect do |line|
 			line.length > options[:line_width] ? line.gsub(/(.{1,#{options[:line_width]}})(\s+|$)/, "\\1\n").strip : line
