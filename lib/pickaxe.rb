18fb8ac48c0e40b881608500cdc0c782e0baeeca
Dawid Fatyga <dawid.fatyga@gmail.com>
2010-12-01 00:28:15 +0100

    Version bumped to 0.4.2

--- lib/pickaxe.rb
+++ lib/pickaxe.rb
@@ -5,7 +5,7 @@ require "active_support/all"
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
 
 module Pickaxe
-	VERSION = "0.4.1"
+	VERSION = "0.4.2"
 	
 	class PickaxeError < StandardError
 		attr_reader :status_code
